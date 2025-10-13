eef5250425244f2d0c0eeff2729feb3d98bb552e
Visyi <simonbahrani@gmail.com>
2025-10-13 02:45:41 +0100

    commit committer Visyi

--- 01.Drawing/sketch.js
+++ 01.Drawing/sketch.js
@@ -5,19 +5,18 @@ function setup() {
 
 function draw() { 
   background(255);
-    fill(128)
-    stroke(64)
-    strokeWeight(4)
+    fill(128);
+    stroke(64);
+    strokeWeight(4);
   rect(450, 200, 300, 50); //Surveillance Base
-  arc (600, 250, 250, 225, 0, PI); //Surveillance Dome
-   fill(50)
-  rect(570, 250, 60, 80, 0, 0, 20, 20)
-  fill(128)
-  stroke(0)
-  strokeWeight(4)
+  arc(600, 250, 250, 225, 0, PI); //Surveillance Dome
+   fill(50);
+  rect(570, 250, 60, 85, 0, 0, 20, 20); //Camera Pocket
+  fill(128);
+  stroke(0);
+  strokeWeight(4);
   circle(600, 310, 50); //Surveillance Camera
-    fill(0)
+    fill(255);
   circle(600, 310, 25); //Surveillance Camera Lens
 
 }
-arc()
