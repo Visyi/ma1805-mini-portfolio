db9f59a560ab7f73de3e3cd39ca86541c567d5cd
Visyi <simonbahrani@gmail.com>
2025-11-05 20:33:01 +0000

    commit

--- 02.Movement/sketch.js
+++ 02.Movement/sketch.js
@@ -1,8 +1,18 @@
-function setup() {
-  createCanvas(window.innerWidth, window.innerHeight);
+let mySound;
+function preload() {
+  soundFormats('mp3');
+  mySound = loadSound('actionloop.mp3');
 }
 
-function draw() {
+function setup() {
+  let cnv = createCanvas(100, 100);
+  cnv.mousePressed(canvasPressed);
   background(220);
-  square(window.innerWidth/2, window.innerHeight/2, 50) 
+  text('tap here to play', 10, 20);
+}
+
+function canvasPressed() {
+  // playing a sound file on a user gesture
+  // is equivalent to `userStartAudio()`
+  mySound.play();
 }
--- 04.Media/sketch.js
+++ 04.Media/sketch.js
@@ -1,8 +1,34 @@
+let soundEffect;
+let img;
+let img2;
+
+function preload() {
+  img = loadImage('samurai.gif');
+  img2 = loadImage('loader.gif');
+  soundFormats('mp3');
+  soundEffect = loadSound('actionloop.mp3');
+  outputVolume(0.3);
+}
+
 function setup() {
   createCanvas(window.innerWidth, window.innerHeight);
+  background(0);
+
+  // try to start sound immediately
+  soundEffect.loop();
 }
 
 function draw() {
-  background(220);
-  square(window.innerWidth/2, window.innerHeight/2, 50) 
+  background(0);
+
+  // main samurai gif
+  push();
+  imageMode(CENTER);
+  image(img, width / 2, height / 2.5, 125, 125);
+  pop();
+
+  // small loader gif in top-right corner
+  imageMode(CORNER);
+  image(img2, width / 1.1, height / 15, 50, 50);
 }
+
